0378e3cd0662fcaffee658e70a3558a99d68a10b
Srilatha <177000222+srilatha940@users.noreply.github.com>
2025-12-11 22:04:44 +0530

    allowedhost

--- aiven_deploy/aiven_deploy/settings.py
+++ aiven_deploy/aiven_deploy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-project-aiven-deploy-purpose-2.onrender.com']
 
 
 # Application definition
